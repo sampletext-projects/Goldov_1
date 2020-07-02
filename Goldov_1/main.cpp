7ebc760a46010d51bcf80ff8deecb75a01018693
sampletext32 <sampletext32@bk.ru>
2020-07-02 18:42:25 +0300

    Add project

---
+++ Goldov_1/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	int n;
+	double x, y = 0, e;
+
+	cout << "Введите X: ";
+	cin >> x;
+	cout << "Введите N: ";
+	cin >> n;
+	cout << "Введите E: ";
+	cin >> e;
+
+	double x_n = pow(x, n);
+	int k = 1;
+	double t_y = 1 / k * x_n;
+
+	while (abs(t_y - y) > e)
+	{
+		y = t_y;
+
+		x_n /= x;
+		k++;
+		t_y += 1 / k * x_n;
+	}
+
+	cout << "Y(" << x << ") = " << y << "\n";
+
+	system("pause");
+}
